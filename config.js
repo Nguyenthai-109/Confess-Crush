0545eabf2e664daa502a49ca2ef7ebc786e73bcf
Nguyenthai-109 <86713376+Nguyenthai-109@users.noreply.github.com>
2022-11-08 11:19:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "gửi đến bạn thân iu",
-    introTitle: 'Nhung !!!',
+    introTitle: 'Chiii !!!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
